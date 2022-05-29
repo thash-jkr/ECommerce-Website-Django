4ed18db62627f91c0ba8b868b8a6c0c316abed66
thash-jkr <afthashsahal123@gmail.com>
2022-05-29 20:19:32 +0530

    added sessions

--- basket/basket.py
+++ basket/basket.py
@@ -1,7 +1,16 @@
 class Basket:
+
     def __init__(self, request):
         self.session = request.session
         basket = self.session.get("skey")
         if "skey" not in request.session:
-            basket = self.session["skey"] = {"number": 109677}
+            basket = self.session["skey"] = {}
         self.basket = basket
+
+    def add(self, product):
+        product_id = product.id
+
+        if product_id not in self.basket:
+            self.basket[product_id] = {"price": str(product.price)}
+
+        self.session.modified = True
--- basket/views.py
+++ basket/views.py
@@ -1,6 +1,19 @@
-import re
+from django.http import JsonResponse
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+
+from .basket import Basket
+from store.models import Product
 
 # Create your views here.
 def basket_summary(request):
     return render(request, "store/basket/summary.html")
+
+def basket_add(request):
+    basket = Basket(request)
+    if request.POST.get("action") == "post":
+        product_id = int(request.POST.get("productid"))
+        product = get_object_or_404(Product, id=product_id)
+        basket.add(product=product)
+        response = JsonResponse({"test": "data"})
+        return response
